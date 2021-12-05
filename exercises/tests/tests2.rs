5fb5b5773e4d258732460f0dc525c67b5c9854e0
Jhon Rocha <jhmrocha@gmail.com>
2021-12-05 14:58:45 -0300

    Done: Tests and Quiz3

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,16 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(1,1);
+    }
+    fn fail() {
+        assert_eq!(2,1);
+    }
+    fn neq() {
+        assert_ne!(2,1);
     }
 }
